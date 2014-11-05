cfa26ffa4cc5032cd83c1d7fbb5693036eb67dbb
Kunal Vyas <kv.kunalvyas@gmail.com>
2014-11-04 22:00:32 -0500

    a little more clean up

---
+++ source/CMakeFiles/video.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video.dir/video.cpp.o"
+  "video.pdb"
+  "video"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/video.cpp
@@ -0,0 +1,59 @@
+// Eugene Stanley
+// 9/29/14
+// eugene.stanley@gmail.com
+
+// This program is based on code provided by Dr. Kate Saenko of UMass Lowell
+// This file has been modified to normalize the image using the grey world
+// algorithm prior to conversion to HSV and color detection
+
+#include <iostream>
+#include <stdio.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    // open the video file for reading
+    VideoCapture cap("vid2.mp4");
+    
+    // if not success, exit program
+    while(!cap.isOpened())
+    {
+        cout << "Cannot open the video file" << endl;
+        
+    }
+    
+    //get the frames per seconds of the video
+    double fps = cap.get(CV_CAP_PROP_FPS);
+    cout << "Frame per seconds : " << fps << endl;
+    
+    //create a window called "MyVideo"
+    namedWindow("MyVideo",CV_WINDOW_AUTOSIZE);
+    
+    while(1)
+    {
+        Mat frame;
+        bool bSuccess = cap.read(frame); // read a new frame from video
+        if (!bSuccess) //if not success, break loop
+        {
+            cout << "Cannot read the frame from video file" << endl;
+            break;
+        }
+        
+        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+        
+        if(waitKey(30) == 27) //wait for 'esc' key press for 30 ms. If 'esc' key is pressed, break loop
+        {
+            cout << "esc key is pressed by user" << endl;
+            break;
+        }
+    }
+    
+    return 0;
+    
+}
+
